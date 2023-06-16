d453c24a284a057da990969481d11fcc6c336c1d
Michael Wang <michael.wang@hey.com>
2023-06-16 14:05:20 +0800

    Start to separate data source

--- lib/infra/data_source.dart
+++ lib/infra/data_source.dart
@@ -1,7 +1,5 @@
-import 'dart:convert';
-
 import 'package:flutter_list_riverpod/domain/photo.dart';
-import 'package:http/http.dart' as http;
+import 'package:flutter_list_riverpod/infra/remote_source.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'data_source.g.dart';
@@ -9,12 +7,9 @@ part 'data_source.g.dart';
 @riverpod
 FutureOr<(List<Photo>, Object? err)> dataSource(DataSourceRef ref,
     {required int start, int limit = 20}) async {
-  final resp = await http.get(Uri.parse(
-      'http://jsonplaceholder.typicode.com/photos?_start=$start&_limit=$limit'));
-  if (resp.statusCode != 200) {
-    return (<Photo>[], Exception(resp.reasonPhrase));
-  }
-
-  final photos = Photo.fromJsonList(jsonDecode(resp.body));
-  return (photos, null);
+  // TODO read from local source.
+  // TODO if local source has no data, get from remote source.
+  // TODO save data in local source.
+  return await ref
+      .watch(remoteSourceProvider(start: start, limit: limit).future);
 }
